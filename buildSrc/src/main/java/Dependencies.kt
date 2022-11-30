f1d97332a079fdfad2511b605742caf68c16a6ca
atifereydooni <am.fereydooni@gmail.com>
2022-11-30 02:31:41 +0100

    Add buildSrc for gradle management

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,16 @@
+object Dependencies {
+    const val androidxCore = "androidx.core:core-ktx:${Versions.androidxCore}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val androidMaterial = "com.google.android.material:material:${Versions.androidMaterial}"
+    const val androidxLifecycle = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.androidxLifecycle}"
+
+    //compose
+    const val activityCompose = "androidx.activity:activity-compose:${Versions.activityCompose}"
+    const val composeUI = "androidx.compose.ui:ui:${Versions.compose}"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.compose}"
+
+    // testing
+    const val jUnit = "junit:junit:${Versions.jUnit}"
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,17 @@
+object Versions {
+    const val kotlinCompilerExtensionVersion = "1.3.2"
+    const val kotlin = "1.6.10"
+
+    const val androidxCore = "1.9.0"
+    const val appCompat = "1.5.1"
+    const val androidMaterial = "1.7.0"
+    const val androidxLifecycle = "2.5.1"
+
+    //compose
+    const val activityCompose = "1.6.1"
+    const val compose = "1.3.1"
+    const val composeUiVersion = "1.7.20"
+
+    // testing
+    const val jUnit = "4.13.2"
+}
